2189c2744edd5f4775a68f9b2f7b5dc4dbb70428
Karim Akhtiamov <karim@Karims-MacBook-Air.local>
2026-04-05 21:55:49 -0700

    removed await and sphere works nicely 16, in 1.2 seconds! but i think if the server crashes everything explodes, will add checks from claude

--- planets/test_planet/test_planet.gd
+++ planets/test_planet/test_planet.gd
@@ -8,13 +8,13 @@ func _ready() -> void:
 	var time = Time.get_ticks_msec()
 	
 	await chunks.init()
-	
+
 	# init all chunks
-	await chunks.add_all_chunks()
-	
+	chunks.add_all_chunks()
+
 	# generate all chunks
 	for i in range(chunks.NUM_CHUNKS):
-		await chunks.load_chunk(i)
+		chunks.load_chunk(i)
 	
 	var elapsed = (Time.get_ticks_msec()-time)/1000.0
 	print("Terrain generated in: " + str(elapsed) + "s")
